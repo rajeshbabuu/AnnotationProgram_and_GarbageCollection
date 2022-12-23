5e930b1036f0baabddf2bc1f9d1b4e68647c19c3
Rajesh kumar <rajeshbabu656656@gmail.com>
2022-12-23 10:39:55 +0530

    Employee class validation

---
+++ AnnotationDemo/AnnotationDemo/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace AnnotationDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Annotations Demo");
+            AnnotationsTest.Display();
+        }
+    }
+}
